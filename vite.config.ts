5d80ecaffae95dbbe52c5427b7c6b98b00f5ce8a
jsantos <jsantos@tranzend.com.ph>
2024-12-25 21:40:29 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,6 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  // base: "./", // Update if using a custom domain or subdirectory
-  // build: {
-  //   outDir: "dist",
-  //   emptyOutDir: true,
-  // },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
