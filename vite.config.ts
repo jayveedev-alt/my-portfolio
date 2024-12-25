928d9ef20eb07edcda9fa68e6c434bd2ed4d335b
jsantos <jsantos@tranzend.com.ph>
2024-12-25 19:10:27 +0800

    fix error on deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      // "@": path.resolve(__dirname, "./src"),
+      '@': '/src',
     },
   },
   assetsInclude: ['**/*.PNG', '**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.gif', '**/*.svg'],
